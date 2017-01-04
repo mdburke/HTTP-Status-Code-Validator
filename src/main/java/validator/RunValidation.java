b0595b06ae3c5425198dad171a54890b8f7f6d7c
Michael Burke <mike@pixotech.com>
2017-01-04 16:25:56 -0600

    Make validation tests dynamic. Change name.

---
+++ src/main/java/validator/RunValidation.java
@@ -0,0 +1,42 @@
+package validator;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
+
+@RunWith(Parameterized.class)
+public class RunValidation {
+
+    private String urlUnderTest;
+
+    public RunValidation(String urlUnderTest) {
+        this.urlUnderTest = urlUnderTest;
+    }
+
+    @Parameterized.Parameters()
+    public static Iterable<Object[]> generateParameters() {
+        List<Object[]> result = new ArrayList<>();
+        UrlList urlList = new UrlList("TestUrls.csv");
+        for (String url : urlList.getUrlList()) {
+            result.add(new Object[] { url });
+        }
+
+        return result;
+    }
+
+    @Test
+    public void testUrl() {
+        StatusCodeValidatorSingle validator = new StatusCodeValidatorSingle(urlUnderTest);
+
+        try {
+            Assert.assertEquals(200, validator.checkURLStatusCode());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/validator/UrlList.java
+++ src/main/java/validator/UrlList.java
@@ -44,6 +44,4 @@ public class UrlList {
             }
         }
     }
-
-//    private void
 }
