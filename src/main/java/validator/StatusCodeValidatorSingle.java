830f104416e400cc7e0b942d2dc53590bd1ef826
Michael Burke <mike@pixotech.com>
2017-01-06 09:38:43 -0600

    Remove the html/css logs due to htmlunit.

--- src/main/java/validator/StatusCodeValidatorSingle.java
+++ src/main/java/validator/StatusCodeValidatorSingle.java
@@ -3,6 +3,9 @@ package validator;
 import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
 import com.gargoylesoftware.htmlunit.WebClient;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger
+import org.apache.commons.logging.LogFactory;
 
 
 public class StatusCodeValidatorSingle {
@@ -14,7 +17,14 @@ public class StatusCodeValidatorSingle {
     }
 
     public int checkURLStatusCode() throws IOException {
+        /* Remove annoying logs */
+        LogFactory.getFactory().setAttribute("org.apache.commons.logging.Log",
+                "org.apache.commons.logging.impl.NoOpLog");
+        Logger.getLogger("com.gargoylesoftware.htmlunit").setLevel(Level.OFF);
+        Logger.getLogger("org.apache.commons.httpclient").setLevel(Level.OFF);
+
         WebClient webClient = new WebClient();
+
         int statusCode;
 
         try {
